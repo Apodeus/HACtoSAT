1c66db55abcae61a0059eb6852bbe645979bf9a6
Apodeus <ordonez.romain@gmail.com>
2017-11-03 15:24:33 +0100

    Rename some vars

--- main.c
+++ main.c
@@ -86,8 +86,8 @@ void number4(SAT_Formula f, int** vars, int k){
 void printResultGraph(int nbVertex, int k){
 
     //init vars and stuff
-    char* var = (char*) malloc(sizeof(char) * 255);
-    int* r = (int*) malloc(sizeof(int) * nbVertex);
+    char* varString = (char*) malloc(sizeof(char) * 255);
+    int* positiveVars = (int*) malloc(sizeof(int) * nbVertex);
 
     //malloc of the adjacent matrix & fill with 0
     int** result = (int**) malloc(sizeof(int*) * nbVertex);
@@ -97,46 +97,46 @@ void printResultGraph(int nbVertex, int k){
             result[i][j] = 0;
     }
 
-    int i = 0;
 
     //get all the positives values from stdin
-    while (var[0] != '0'){
-        scanf("%s", var);
-        int n = atoi(var);
-        if (n > 0){
-            r[i] = n;
+    int i = 0;
+    while (varString[0] != '0'){
+        scanf("%s", varString);
+        int var = atoi(varString);
+        if (var > 0){
+            positiveVars[i] = var;
             i++;
         }
     }
 
     //translate values from Glucz to vertex identifier + their d(v)
-    int* d = (int*) malloc(sizeof(int) * nbVertex);
-    int* v = (int*) malloc(sizeof(int) * nbVertex);
+    int* heightVertex = (int*) malloc(sizeof(int) * nbVertex);
+    int* markOnVertex = (int*) malloc(sizeof(int) * nbVertex);
     for (i = 0; i < nbVertex; i++){
-        d[i] = (r[i] - 1) % (k + 1);
-        v[i] = 0;
+        heightVertex[i] = (positiveVars[i] - 1) % (k + 1);
+        markOnVertex[i] = 0;
     }
 
-    int degree = 0; //root
+    int height = 0; //root
     queue q = newQueue(0);
-    queue first = q;
+    queue first = q; //queue first is just here for free the queue
     int root_i = -1;
     while (q != NULL){
-        queue t = q;
+        /*queue t = q;
         while (t!= NULL){
             t = t->next;
         }
-
+        */
         for (i = 0; i < nbVertex; i++){
-            if (d[i] == degree){
-                if (degree == 0 && root_i == -1)
+            if (heightVertex[i] == height){
+                if (height == 0 && root_i == -1)
                     root_i = i;
                 addToQueue(q, i);
-                if (q != NULL && v[i] == 0 && are_adjacent(q->value, i)){
+                if (q != NULL && markOnVertex[i] == 0 && are_adjacent(q->value, i)){
                     fprintf(stderr, "v%d and v%d are adjacent\n", q->value, i);
                     result[i][q->value] = 1;
                     result[q->value][i] = 1;
-                    v[i] = 1;
+                    markOnVertex[i] = 1;
                 }
             }
         }
@@ -145,9 +145,9 @@ void printResultGraph(int nbVertex, int k){
         if (q == NULL)
             break;
 
-        int new_degree = d[q->value] + 1;
+        int new_height = heightVertex[q->value] + 1;
 
-        degree = new_degree;
+        height = new_height;
     }
 
     //display
@@ -161,10 +161,10 @@ void printResultGraph(int nbVertex, int k){
     }
 
     destroyQueue(first);
-    free(v);
-    free(d);
-    free(var);
-    free(r);
+    free(markOnVertex);
+    free(heightVertex);
+    free(varString);
+    free(positiveVars);
     for(int i = 0; i < nbVertex; i ++){
       free(result[i]);
     }
